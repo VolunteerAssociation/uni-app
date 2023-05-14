feead213d152e27d6109c9f791ff9383db677372
chenguihui1 <2404296720@qq.com>
2023-05-14 14:01:15 +0800

    版本更新

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -7,46 +7,67 @@
  * @filePath: Do not edit
 -->
 <template>
-	<view class="Home">
-		<view class="home-page-header">
-			<view class="search" v-bind="navToSearchPage">
-				<!-- <t-search t-class-input="t-search__input" t-class-input-container="t-search__input-container"
-					placeholder="iphone 13 火热发售中" leftIcon="" disabled> -->
-					<icon slot="left-icon" prefix="wr" name="search" size="40rpx" color="#bbb" />
-				<!-- </t-search> -->
-			</view>
-			<view class="swiper-wrap"></view>
-		</view>
-		<view class="home-container"></view>
-	</view>
+  <view>
+    <ad-rewarded-video ref="adRewardedVideo" adpid="1507000689" :preload="false" :loadnext="false" :disabled="true"
+      v-slot:default="{loading, error}" @load="onadload" @close="onadclose" @error="onaderror">
+      <view class="ad-error" v-if="error">{{error}}</view>
+    </ad-rewarded-video>
+    <button type="primary" :disabled="isLoading" :loading="isLoading" @click="showAd">显示广告</button>
+  </view>
 </template>
 
 <script>
-import {
-	carousel
-} from "@/api/home";
-import RightContent from "@/components/RightContent/RightContent"
-export default {
-	comments: {
-		RightContent
-	},
-	data() {
-		return {
-			title: "Home"
-		}
-	},
-	onLoad() {
-	},
-	methods: {
-		async getUser() {
-			let result = await carousel({});
-			console.log(result);
-		}
-	}
-
-}
+  export default {
+    data() {
+      return {
+        isLoading: false
+      }
+    },
+    onReady() {
+			try {
+				this.isLoading = true;
+      this.$refs.adRewardedVideo.load();
+			} catch (err) {
+				console.log(err)
+			}
+      
+    },
+    methods: {
+      showAd() {
+        if (this.isLoading) {
+          return
+        }
+        this.$refs.adRewardedVideo.show();
+      },
+      onadload(e) {
+        this.isLoading = false;
+        console.log('广告数据加载成功');
+      },
+      onadclose(e) {
+        const detail = e.detail
+        // 用户点击了【关闭广告】按钮
+        if (detail && detail.isEnded) {
+          // 正常播放结束
+          console.log("onClose " + detail.isEnded);
+        } else {
+          // 播放中途退出
+          console.log("onClose " + detail.isEnded);
+        }
+        //this.isLoading = true;
+        //this.$refs.adRewardedVideo.load();
+      },
+      onaderror(e) {
+        // 广告加载失败
+        console.log(e.detail);
+        this.isLoading = false;
+      }
+    }
+  }
 </script>
 
-<style lang="less">
-@import url("./home.less");
+<style>
+  .ad-error {
+    color: orangered;
+    margin-top: 5px;
+  }
 </style>
